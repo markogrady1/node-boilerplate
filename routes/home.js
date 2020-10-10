e950061383bcd385f4c1726f4fa29476efc183fb
Mark O Grady <mark.o'grady@broadbean.com>
2020-10-10 20:16:14 +0100

    add rendering example

--- routes/home.js
+++ routes/home.js
@@ -3,11 +3,12 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-// all boards, optionally filtered
 router.get('/', async (req, res) => {
-
     res.render('home', {
-        data: 'Hello Universe'
+        data: {
+            greeting: 'Hello Universe',
+            time: new Date().toJSON()
+        }
     });
 });
 
